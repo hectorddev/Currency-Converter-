36626cae7e036843847579c3aa7f94b6e5974c12
Andrew Molina <rasend20@gmail.com>
2021-08-09 23:47:24 -0700

    regresando todo a la normalidad

--- Scraper/Scraper/spiders/run.py
+++ Scraper/Scraper/spiders/run.py
@@ -3,6 +3,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from functions import verify, writeCsv, verify_folder
 
+
 JSON_PATH = r'../../../Json_files/'
 JSON_FILENAME = 'mean_currencies.json'
 CSV_PATH = r'../../../history_files/'
@@ -11,14 +12,11 @@ CSV_FILENAME = 'mean_history.csv'
 def task():
     verify_folder()
     verify()
-    try:
-        proccess = CrawlerProcess(get_project_settings())
-        proccess.crawl(usd_cop)
-        proccess.crawl(usd_btc)
-        proccess.crawl(usd_ves)
-        proccess.start()
-    except Exception as e:
-        print(e)    
+    proccess = CrawlerProcess(get_project_settings())
+    proccess.crawl(usd_cop)
+    proccess.crawl(usd_btc)
+    proccess.crawl(usd_ves)
+    proccess.start()    
     writeCsv(JSON_PATH,JSON_FILENAME,CSV_PATH,CSV_FILENAME)
 
 task()
