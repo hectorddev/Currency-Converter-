034617d72e2a55a9bd80b094ff0f4da82380c4c1
Hector Marquez <hectordmv21@gmail.com>
2021-08-11 07:17:37 -0430

    Change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let { exec } = require('child_process')
 const path = require('path')
-const ruta = path.join(__dirname, './Scraper/Scraper/spiders/run.py')
+const ruta = path.join(__dirname, '/Scraper/Scraper/spiders/run.py')
 
 
 exec(ruta, function(err){
