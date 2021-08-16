0c213d6714398adab70b9907e39ef764bce78dcb
Hector Marquez <hectordmv21@gmail.com>
2021-08-16 16:03:11 -0430

    Cambio minimo0

--- Scraper/Scraper/settings.py
+++ Scraper/Scraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'Scraper.spiders'
 USER_AGENT = 'Mozila/5.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
